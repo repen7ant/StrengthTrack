e55f5eab921eadf68f274f6943f631e0cd67a47c
repen7ant <ilyarepentant@icloud.com>
2026-01-11 18:49:26 +0500

    removed excess code from core/views.py

--- core/views.py
+++ core/views.py
@@ -3,13 +3,4 @@ from django.shortcuts import render
 
 def home(request):
     """Главная страница"""
-    context = {
-        "title": "StrengthTrack - Система планирования тренировок",
-        "features": [
-            "Планирование тренировок по принципам RIR/RPE",
-            "Автоматическая генерация мезоциклов",
-            "Отслеживание прогресса",
-            "Визуализация результатов",
-        ],
-    }
-    return render(request, "core/home.html", context)
+    return render(request, "core/home.html")
